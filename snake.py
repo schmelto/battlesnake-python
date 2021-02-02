9504a97fbf202df7d9bb2dcf78004d13954951ac
schmelto <tom.schmelzer@web.de>
2021-02-02 18:50:45 +0100

    update snake.py with additional paramters

--- snake.py
+++ snake.py
@@ -4,12 +4,10 @@ class Snake():
     def __init__(self, snake):
         self.id = snake['id'] 
         self.name = snake['name']
+        self.latency = snake['latency']
         self.health = snake['health']
         self.body = snake['body']
         self.head = snake['head']
         self.length = snake['length']
-
-
-        # self.head = self.coordinates[0]
-        # self.length = len(self.coordinates)
+        self.shout = snake['shout']
 
